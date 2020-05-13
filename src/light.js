31feed2bfb6f72a696c5eb11e68ebe3209e2d131
Alexander Kachkaev <alexander@kachkaev.ru>
2020-05-13 20:56:52 +0100

    Run "npm run fix"

--- src/light.js
+++ src/light.js
@@ -1,32 +1,32 @@
 const me = {
-    marginRight: '20%',
-    width:'100px',
-    height:'100px',
-    float:'right'
-}
+  marginRight: "20%",
+  width: "100px",
+  height: "100px",
+  float: "right",
+};
 const about = {
-    textAlign: 'left',
-    marginLeft: '30%'
-}
+  textAlign: "left",
+  marginLeft: "30%",
+};
 const a = {
-    //textDecoration: 'none',
-    color:'black'
-}
+  //textDecoration: 'none',
+  color: "black",
+};
 const app = {
-    backgroundColor:'white',
-    color:'black',
-    textAlign: 'center',
-    fontFamily: 'Montserrat'
-}
+  backgroundColor: "white",
+  color: "black",
+  textAlign: "center",
+  fontFamily: "Montserrat",
+};
 const languages = {
-    float:'right',
-    marginRight:'5%'
-}
+  float: "right",
+  marginRight: "5%",
+};
 let light = {
-    a:a,
-    about:about,
-    app:app,
-    me:me,
-    languages:languages
-}
+  a: a,
+  about: about,
+  app: app,
+  me: me,
+  languages: languages,
+};
 export default light;
